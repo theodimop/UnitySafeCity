7a5493ace9e92ae26ddde723d63202054ddf1358
thodwrisDimopoulos <dj_diteo@hotmail.com>
2015-10-26 01:34:21 +0200

    op
    Basics

---
+++ Assets/OpenDataScreen/DropDownMenuHandle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DropDownMenuHandle : MonoBehaviour {
+
+
+
+    
+
+    public Text textCounty,textDay,textMonth,textYear,textAccidentType;    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Listener for button Show me Data
+    public void buttonListenerShowMeData()
+    {
+
+        Debug.Log(textCounty.text + " " + textDay.text + "" + textMonth.text + "" + textYear.text+""+textAccidentType.text);        
+    }
+}
